fbbc1c608e1d20f003b2ab33d6929bce614376e8
Daniel Friedenbach <danielfriedenbach@gmail.com>
2015-07-27 20:58:47 -0700

    Implement Quadtree.add and Quadtree.find

--- quadtree.js
+++ quadtree.js
@@ -3,6 +3,8 @@ var Quadtree = function(x, y){
     throw new Error('You must supply coordinate to the initial node!');
   }
   this.coordinates = [x, y];
+  this.x = x;
+  this.y = y;
   this.children = {
     topRight: null,
     topLeft: null,
@@ -12,13 +14,70 @@ var Quadtree = function(x, y){
 }
 
 Quadtree.prototype.add = function(x, y){
-  /* Adds a new node to the tree with the given coordinates */
-
+  if(y >= this.y) {
+    if (x >= this.x) {
+      if (this.children.topRight) {
+        this.children.topRight.add(x, y);
+      } else {
+        this.children.topRight = new Quadtree(x, y);
+      }
+    } else {
+      if (this.children.topLeft) {
+        this.children.topLeft.add(x, y);
+      } else {
+        this.children.topLeft = new Quadtree(x, y);
+      }
+    }
+  } else {
+    if (x >= this.x) {
+      if (this.children.bottomRight) {
+        this.children.bottomRight.add(x, y);
+      } else {
+        this.children.bottomRight = new Quadtree(x, y);
+      }
+    } else {
+      if (this.children.bottomLeft) {
+        this.children.bottomLeft.add(x, y);
+      } else {
+        this.children.bottomLeft = new Quadtree(x, y);
+      }
+    }
+  }
 }
 
 Quadtree.prototype.find = function(x, y){
-  /* Returns either the node with the given coordinates or null */
-
+  if(y === this.y && x === this.x) {
+    return this;
+  }
+  if(y >= this.y) {
+    if (x >= this.x) {
+      if (this.children.topRight) {
+        return this.children.topRight.find(x, y);
+      } else {
+        return null;
+      }
+    } else {
+      if (this.children.topLeft) {
+        return this.children.topLeft.find(x, y);
+      } else {
+        return null;
+      }
+    }
+  } else {
+    if (x >= this.x) {
+      if (this.children.bottomRight) {
+        return this.children.bottomRight.find(x, y);
+      } else {
+        return null;
+      }
+    } else {
+      if (this.children.bottomLeft) {
+        return this.children.bottomLeft.find(x, y);
+      } else {
+        return null;
+      }
+    }
+  }
 }
 
 Quadtree.prototype.findInRange = function(topRight, topLeft, bottomLeft, bottomRight){
